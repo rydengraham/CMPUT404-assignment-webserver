1452c7a93b00fbc44af9f6d8ea67aca3ee87ee65
rydengraham <ryden3.14@gmail.com>
2023-01-24 00:49:22 -0700

    Modified server and added screenshots

--- server.py
+++ server.py
@@ -1,7 +1,10 @@
 #  coding: utf-8 
 import socketserver
+import os
+import mimetypes
 
 # Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Modified 2023 by Ryden Graham
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,13 +29,74 @@ import socketserver
 
 # try: curl -v -X GET http://127.0.0.1:8080/
 
+# 404 error template, answered by linusg: https://stackoverflow.com/questions/41852380/how-to-abort-a-python-script-and-return-a-404-error
+GENERIC_404_ERROR = b"HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><h1>404 Not Found</h1></body></html>"
+GENERIC_405_ERROR = b"HTTP/1.1 405 Not Supported\r\nContent-Type: text/html\r\n\r\n<html><head></head><body><h1>405 Method Not Supported</h1></body></html>"
 
 class MyWebServer(socketserver.BaseRequestHandler):
-    
+
+    def createFoundHttpReturn(self, header, body):
+        return b"HTTP/1.1 200 OK Found\r\n" + header + b"\r\n\r\n" + body
+
+    def createRedirectReturn(self, header):
+        return b"HTTP/1.1 301 Moved Permanently\r\n" + header + b"\r\n\r\n<html><head></head><body><h1>301 Moved Permanently</h1></body></html>"
+
+    def handleRequest(self):
+        path = self.path.strip()
+
+        # offset into www folder
+        absFilePath = "www" + path.decode("utf-8")
+
+        # Check if file type is legal. We support HTML and CSS files only. We also support paths ending in /.
+        if not absFilePath.endswith(("/", ".html", ".css")):
+
+            # Add '/' here to check the folder
+            absFilePath = absFilePath + "/"
+            # Check if we need to redirect
+            if not os.path.exists(absFilePath):
+                return GENERIC_404_ERROR
+
+            # Send redirect
+            redirectHeader = b"Location: " + path + b"/\r\nContent-type: text/html"
+            return self.createRedirectReturn(redirectHeader)
+
+        #support paths ending in /
+        if absFilePath.endswith("/"):
+            absFilePath += "index.html"
+
+        try:
+            fh = open(absFilePath, 'r', encoding="utf-8")
+            fileData = fh.read()
+            fh.close()
+
+            fileType, _ = mimetypes.guess_type(absFilePath)
+
+            # Add content type header, answered by Vikas Ojha: https://stackoverflow.com/questions/32651362/how-do-i-set-the-content-type-for-post-requests-in-python-requests-library
+            header = b"Content-type: " + fileType.encode("utf-8")
+
+            response = self.createFoundHttpReturn(header, fileData.encode("utf-8"))
+
+            return response
+        except IOError:
+            return GENERIC_404_ERROR
+  
     def handle(self):
         self.data = self.request.recv(1024).strip()
-        print ("Got a request of: %s\n" % self.data)
-        self.request.sendall(bytearray("OK",'utf-8'))
+
+        httpInfo = self.data.split()
+
+        self.method = httpInfo[0]
+        self.connectionInfo = httpInfo[6]
+        
+        if self.method.strip() == b"GET":
+            self.path = httpInfo[1]
+
+            fileData = self.handleRequest()
+
+            self.request.sendall(fileData)
+        else:
+            # don't support other requesttypes
+            self.request.sendall(GENERIC_405_ERROR)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 8080
